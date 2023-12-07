f0335c6898d6acaa3e829b93a4e639726fdfb1e2
= <=>
2023-12-07 22:26:42 +0100

    Hotfix 0.2

--- index.ts
+++ index.ts
@@ -16,14 +16,15 @@ initMessagesEvents();
 client.on("interactionCreate", async (interaction: any) => {
     if (!interaction.isChatInputCommand) return;
 
-    if (interaction.commandName === "premia") calculateBonus(interaction, interaction.options.getNumber("kwota"), interaction.options.getString("zwrot"));
+    try {
+        if (interaction.commandName === "premia") calculateBonus(interaction, interaction.options.getNumber("kwota"), interaction.options.getString("zwrot"));
 
-    if (interaction.commandName === "register") addUserData(interaction.user.id, interaction.options.getString("imie_nazwisko"), interaction.options.getString("phone"), interaction.options.getString("bank_acc"), interaction);
+        if (interaction.commandName === "register") addUserData(interaction.user.id, interaction.options.getString("imie_nazwisko"), interaction.options.getString("phone"), interaction.options.getString("bank_acc"), interaction);
 
-    if (interaction.commandName === "send-panel") sendPanel(interaction);
+        if (interaction.commandName === "send-panel") sendPanel(interaction);
 
-    if (interaction.commandName === "unregister") removeUserData(interaction.options.getString("user_id"), interaction);
-
+        if (interaction.commandName === "unregister") removeUserData(interaction.options.getString("user_id"), interaction);
+    } catch (err) { console.log(err) }
 });
 
 client.login(process.env.TOKEN);
